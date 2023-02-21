bf902bdb778dfba52dd0911f4a4c38d2abd7eb63
leoCottret <71428793+leoCottret@users.noreply.github.com>
2023-02-21 18:02:41 +0100

    add init commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# What is it?
+- Those are the different exercises from this course: 
---
+++ Section_2/learn-spring-boot-1/src/test/java/com/in28minutes1/learnspringboot1/LearnSpringBoot1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minutes1.learnspringboot1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringBoot1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
