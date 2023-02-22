23554fcb000d664bfaa516769cc06a7d25153cbe
leoCottret <71428793+leoCottret@users.noreply.github.com>
2023-02-22 23:50:29 +0100

    Autowiring (S2_10)

--- Section_2/learn-spring-boot-1/src/main/java/com/in28minutes1/learnspringboot1/App02HelloWorldSpring.java
+++ Section_2/learn-spring-boot-1/src/main/java/com/in28minutes1/learnspringboot1/App02HelloWorldSpring.java
@@ -6,7 +6,6 @@ public class App02HelloWorldSpring {
 
 	public static void main(String[] args) {
 
-
 		// 1: Launch a Spring Context
 		var context = new AnnotationConfigApplicationContext(HelloWorldConfiguration.class);
 		
@@ -16,8 +15,9 @@ public class App02HelloWorldSpring {
 		System.out.println(context.getBean("name"));
 		System.out.println(context.getBean("age"));
 		System.out.println(context.getBean("person"));
-		System.out.println(context.getBean("address"));
-		
+		System.out.println(context.getBean("person2MethodCall"));
+		System.out.println(context.getBean("person3Parameters"));
+		System.out.println(context.getBean("address_custom"));
 	}
 
 }
--- Section_2/learn-spring-boot-1/src/main/java/com/in28minutes1/learnspringboot1/HelloWorldConfiguration.java
+++ Section_2/learn-spring-boot-1/src/main/java/com/in28minutes1/learnspringboot1/HelloWorldConfiguration.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Configuration;
 // record (released in JDK 16)
 // eliminate verbosity in creating Java Beans
 // public accessor methods, constructor, equals, hashcode and toString are automatically created
-record Person(String name, int age) {};
+record Person(String name, int age, Address address) {};
 record Address(String firstLine, String city) {};
 
 @Configuration
@@ -25,11 +25,31 @@ public class HelloWorldConfiguration {
 	
 	@Bean
 	public Person person() {
-		return new Person("Léa", 16);
+		return new Person("Léa", 16, address_custom());
 	}
 	
 	@Bean
-	public Address address() {
+	public Person person2MethodCall() {
+		return new Person(name(), age(), address_custom_2());//name, age
+	}
+	
+	@Bean
+	public Person person3Parameters(String name, int age, Address address_custom_3) {
+		return new Person(name, age, address_custom_3);//name, age, address_custom
+	}
+	
+	@Bean(name = "address_custom")
+	public Address address_custom() {
 		return new Address("Baker Street", "London");
+	}
+	
+	@Bean(name = "address_custom_2")
+	public Address address_custom_2() {
+		return new Address("Baker Street2", "London");
+	}
+	
+	@Bean(name = "address_custom_3")
+	public Address address_custom_3() {
+		return new Address("Baker Street3", "London");
 	}
 }
