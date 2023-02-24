e08369af8a7562182425949eb7187d6696ba03e5
leoCottret <71428793+leoCottret@users.noreply.github.com>
2023-02-24 12:23:58 +0100

    Dependency Injection Types S03_04

--- src/learn-spring-boot-02/src/main/java/com/in28minutes/learnspringboot/examples/a1/DependencyInjectionApplication.java
+++ src/learn-spring-boot-02/src/main/java/com/in28minutes/learnspringboot/examples/a1/DependencyInjectionApplication.java
@@ -1,18 +1,23 @@
-package com.in28minutes.learnspringboot.game;
+package com.in28minutes.learnspringboot.examples.a1;
+
+import java.util.Arrays;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+import com.in28minutes.learnspringboot.game.GameRunner;
+
 @Configuration
-@ComponentScan("com.in28minutes1.learnspringboot1")
+@ComponentScan
 public class DependencyInjectionApplication {
 	
 	public static void main(String[] args) {
 		
 		try (var context = new AnnotationConfigApplicationContext(DependencyInjectionApplication.class)) {
-			context.getBean(GameRunner.class).run();	
+			Arrays.stream(context.getBeanDefinitionNames())
+				.forEach(System.out::println);
 		}
 		
 	}
