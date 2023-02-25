6359bd3c958c500a5d655339c7e5bf1db62253b6
leoCottret <71428793+leoCottret@users.noreply.github.com>
2023-02-25 21:18:18 +0100

    Prototype vs Singleton S04_03

---
+++ src/learn-spring-boot-02/src/main/java/com/in28minutes/learnspringboot/examples/a3/prototypevssingleton/BeanScopesLauncherApplication.java
@@ -0,0 +1,48 @@
+package com.in28minutes.learnspringboot.examples.a3.prototypevssingleton;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import com.in28minutes.learnspringboot.game.GameRunner;
+
+@Component
+class NormalClass {
+	
+}
+
+@Scope(value=ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+@Component
+class PrototypeClass {
+	
+}
+
+@Configuration
+@ComponentScan
+public class BeanScopesLauncherApplication {
+	
+	public static void main(String[] args) {
+		
+		try (var context = new AnnotationConfigApplicationContext(BeanScopesLauncherApplication.class)) {
+
+			// The bean will have the same memory address, they are the same object (singleton)
+			System.out.println(context.getBean(NormalClass.class));
+			System.out.println(context.getBean(NormalClass.class));
+
+			// The bean will have different memory addresses they are not the same objects (prototype class)
+			System.out.println(context.getBean(PrototypeClass.class));
+			System.out.println(context.getBean(PrototypeClass.class));
+			
+		}
+			
+		
+	}
+
+}
