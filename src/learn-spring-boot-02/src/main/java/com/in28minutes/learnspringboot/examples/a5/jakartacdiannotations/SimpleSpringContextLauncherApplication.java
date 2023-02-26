695d04ae52907ceccabbdd571640626fa0101632
leoCottret <71428793+leoCottret@users.noreply.github.com>
2023-02-26 11:22:40 +0100

    Demo Jakarta CDI injection SB equivalents S04_07

---
+++ src/learn-spring-boot-02/src/main/java/com/in28minutes/learnspringboot/examples/a5/jakartacdiannotations/SimpleSpringContextLauncherApplication.java
@@ -0,0 +1,57 @@
+package com.in28minutes.learnspringboot.examples.a5.jakartacdiannotations;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+import com.in28minutes.learnspringboot.game.GameRunner;
+
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+
+
+//@Component
+@Named
+class BusinessService {
+	DataService dataService;
+
+	public DataService getDataService() {
+		return dataService;
+	}
+
+	//@Autowired
+	@Inject
+	public void setDataService(DataService dataService) {
+		System.out.println("Setter executed");
+		this.dataService = dataService;
+	}
+	
+}
+
+//@Component
+@Named
+class DataService {
+	
+}
+
+@Configuration
+@ComponentScan
+public class SimpleSpringContextLauncherApplication {
+	
+	public static void main(String[] args) {
+		
+		try (var context = new AnnotationConfigApplicationContext(SimpleSpringContextLauncherApplication.class)) {
+			Arrays.stream(context.getBeanDefinitionNames())
+				.forEach(System.out::println);
+			
+			//System.out.println(context.getBean(YourBusinessClass.class));
+		}
+		
+	}
+
+}
