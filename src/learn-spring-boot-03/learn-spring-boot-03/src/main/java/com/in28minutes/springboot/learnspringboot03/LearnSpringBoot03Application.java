068acf2b03d91cfb1d63a544503cffcfa86f277f
leoCottret <71428793+leoCottret@users.noreply.github.com>
2023-02-28 19:41:35 +0100

    add spring boot project (S05_03)

---
+++ src/learn-spring-boot-03/learn-spring-boot-03/src/main/java/com/in28minutes/springboot/learnspringboot03/LearnSpringBoot03Application.java
@@ -0,0 +1,13 @@
+package com.in28minutes.springboot.learnspringboot03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringBoot03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringBoot03Application.class, args);
+	}
+
+}
---
+++ src/learn-spring-boot-03/learn-spring-boot-03/src/test/java/com/in28minutes/springboot/learnspringboot03/LearnSpringBoot03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minutes.springboot.learnspringboot03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringBoot03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
