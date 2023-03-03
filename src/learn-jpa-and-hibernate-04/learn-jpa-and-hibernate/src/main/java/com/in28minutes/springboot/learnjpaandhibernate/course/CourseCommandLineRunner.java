877b336e9434671419e5f8c20d7a118adfc87f9b
leoCottret <71428793+leoCottret@users.noreply.github.com>
2023-03-03 14:23:30 +0100

    Getting started, SPRING JPA (S06_10)

--- src/learn-jpa-and-hibernate-04/learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/CourseCommandLineRunner.java
+++ src/learn-jpa-and-hibernate-04/learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/CourseCommandLineRunner.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import com.in28minutes.springboot.learnjpaandhibernate.CourseSpringDataJpaRepository;
 import com.in28minutes.springboot.learnjpaandhibernate.course.jdbc.CourseJdbcRepository;
 import com.in28minutes.springboot.learnjpaandhibernate.course.jpa.CourseJpaRepository;
 
@@ -13,17 +14,20 @@ public class CourseCommandLineRunner implements CommandLineRunner {
 //	@Autowired
 //	private CourseJdbcRepository repository;
 	
+//	@Autowired
+//	private CourseJpaRepository repository;
+	
 	@Autowired
-	private CourseJpaRepository repository;
+	private CourseSpringDataJpaRepository repository;
 	
 	@Override
 	public void run(String... args) throws Exception {
-		repository.insert(new Course(1, "Learn AWS now - JPA", "in28minutes"));
-		repository.insert(new Course(2, "Learn Azure now", "in28minutes"));
-		repository.insert(new Course(3, "Learn Machine Learning now", "in28minutes"));
-		repository.insert(new Course(4, "Should be deleted", "in28minutes"));
-		repository.deleteById(4);
-		System.out.println(repository.findById(1));
+		repository.save(new Course(1, "Learn AWS now - JPA", "in28minutes"));
+		repository.save(new Course(2, "Learn Azure now", "in28minutes"));
+		repository.save(new Course(3, "Learn Machine Learning now", "in28minutes"));
+		repository.save(new Course(4, "Should be deleted", "in28minutes"));
+		repository.deleteById(4l);
+		System.out.println(repository.findById(1l));
 	}
 	
 	
