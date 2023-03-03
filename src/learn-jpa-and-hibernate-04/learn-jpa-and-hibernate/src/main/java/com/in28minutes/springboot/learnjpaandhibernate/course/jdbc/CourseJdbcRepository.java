6943dd172a38d19655c7fb19b9835f8ec8996554
leoCottret <71428793+leoCottret@users.noreply.github.com>
2023-03-03 12:53:26 +0100

    insert/delete data with Spring JDBC (S06_06)

--- src/learn-jpa-and-hibernate-04/learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/jdbc/CourseJdbcRepository.java
+++ src/learn-jpa-and-hibernate-04/learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/jdbc/CourseJdbcRepository.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import com.in28minutes.springboot.learnjpaandhibernate.course.Course;
+
 @Repository
 public class CourseJdbcRepository {
 	
@@ -13,11 +15,18 @@ public class CourseJdbcRepository {
 	private static String INSERT_QUERY = 
 		"""
 		insert into course (id, name, author)
-				values(1, 'Learn AWS', 'in28minutes');		
+				values(?, ?, ?);		
+		""";
+	private static String DELETE_QUERY = 
+		"""
+		delete from course where id = ?
 		""";
 	
-	public void insert() {
-		// update issue a single query (insert, update, delete...)
-		springJdbcTemplate.update(INSERT_QUERY);
+	public void insert(Course course) {
+		springJdbcTemplate.update(INSERT_QUERY, course.getId(), course.getName(), course.getAuthor());
+	}
+	
+	public void deleteById(long id) {
+		springJdbcTemplate.update(DELETE_QUERY, id);
 	}
 }
