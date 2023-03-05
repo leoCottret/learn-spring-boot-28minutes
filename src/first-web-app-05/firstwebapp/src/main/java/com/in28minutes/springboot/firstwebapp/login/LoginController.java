4ccf856794e4947e985244f23e47bc5abab2f904
leoCottret <71428793+leoCottret@users.noreply.github.com>
2023-03-05 21:45:45 +0100

    Add hardcoded validation of username + password (S07_13)

--- src/first-web-app-05/firstwebapp/src/main/java/com/in28minutes/springboot/firstwebapp/login/LoginController.java
+++ src/first-web-app-05/firstwebapp/src/main/java/com/in28minutes/springboot/firstwebapp/login/LoginController.java
@@ -1,5 +1,6 @@
 package com.in28minutes.springboot.firstwebapp.login;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class LoginController {
 
+	@Autowired
+	private AuthenticationService authenticationService;
+	
 	@RequestMapping(value="login", method = RequestMethod.GET)
 	public String gotoLoginPage() {
 		return "login";
@@ -19,6 +23,12 @@ public class LoginController {
 	@RequestMapping(value="login", method = RequestMethod.POST)
 	public String gotWelcomePage(@RequestParam String name, @RequestParam String password, ModelMap model) {
 		model.put("name", name);
-		return "welcome";
+		
+		if (authenticationService.authenticate(name, password)) {
+			return "welcome";
+		}
+		
+		model.put("errorMessage", "Invalid Credentials, please try again");
+		return "login";
 	}
 }
--- src/first-web-app-05/firstwebapp/src/main/resources/META-INF/resources/WEB-INF/jsp/login.jsp
+++ src/first-web-app-05/firstwebapp/src/main/resources/META-INF/resources/WEB-INF/jsp/login.jsp
@@ -5,11 +5,13 @@
 </head>
 	<body>
 		Login page.
+		<pre>${errorMessage}</pre>
 		<form method="post">
 			Name: <input type="text" name="name">
 			Password: <input type="password" name="password">
 			<input type="submit">
 		</form>
+		
 		
 	</body>
 </html>
