66df0a0f572d5d435e52bf6fef53516fcc3261e6
leoCottret <71428793+leoCottret@users.noreply.github.com>
2023-03-10 20:43:07 +0100

    add todo with TodoService, non persistent addition yet [S07_21]

--- src/first-web-app-05/firstwebapp/src/main/java/com/in28minutes/springboot/firstwebapp/todo/TodoController.java
+++ src/first-web-app-05/firstwebapp/src/main/java/com/in28minutes/springboot/firstwebapp/todo/TodoController.java
@@ -1,5 +1,6 @@
 package com.in28minutes.springboot.firstwebapp.todo;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
@@ -15,23 +17,24 @@ public class TodoController {
 
 	@Autowired
 	private TodoService todoService;
-	
-	@RequestMapping(value="list-todos", method = RequestMethod.GET)
+
+	@RequestMapping(value = "list-todos", method = RequestMethod.GET)
 	public String listAllTodos(ModelMap model) {
 		String name = "Léo";
 		List<Todo> todos = todoService.findByUsername(name);
 		model.addAttribute("name", name);
 		model.addAttribute("todos", todos);
 		return "listTodos";
-	}	
-	
-	@RequestMapping(value="add-todo", method = RequestMethod.GET)
+	}
+
+	@RequestMapping(value = "add-todo", method = RequestMethod.GET)
 	public String showTodoPage() {
 		return "todo";
 	}
-	
-	@RequestMapping(value="add-todo", method = RequestMethod.POST)
-	public String addTodo() {
+
+	@RequestMapping(value = "add-todo", method = RequestMethod.POST)
+	public String addTodo(@RequestParam String description, ModelMap model) {
+		todoService.addTodo((String) model.get("name"), description, LocalDate.now().plusYears(1), false);
 		return "redirect:list-todos";
 	}
 }
--- src/first-web-app-05/firstwebapp/src/main/java/com/in28minutes/springboot/firstwebapp/todo/TodoService.java
+++ src/first-web-app-05/firstwebapp/src/main/java/com/in28minutes/springboot/firstwebapp/todo/TodoService.java
@@ -11,14 +11,20 @@ public class TodoService {
 
 	private static List<Todo> todos = new ArrayList<>();
 
+	private static int todosCount = 0;
+	
 	// to initialise a static variable, you need a static block
 	static {
-		todos.add(new Todo(1, "Léo", "Learn AWS", LocalDate.now().plusYears(1), false));
-		todos.add(new Todo(2, "Léo", "Learn DevOps", LocalDate.now().plusYears(1), false));
-		todos.add(new Todo(3, "Léo", "Learn Full Stack Development", LocalDate.now().plusYears(1), false));
+		todos.add(new Todo(++todosCount, "Léo", "Learn AWS", LocalDate.now().plusYears(1), false));
+		todos.add(new Todo(++todosCount, "Léo", "Learn DevOps", LocalDate.now().plusYears(1), false));
+		todos.add(new Todo(++todosCount, "Léo", "Learn Full Stack Development", LocalDate.now().plusYears(1), false));
 	}
 	
 	public List<Todo> findByUsername(String username) {
 		return todos;
+	}
+
+	public void addTodo(String username, String description, LocalDate targetDate, boolean done) {
+		todos.add(new Todo(++todosCount, username, description, targetDate, done));
 	}
 }
