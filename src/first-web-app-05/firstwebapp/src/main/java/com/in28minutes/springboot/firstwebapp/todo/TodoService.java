558d9fa57a7223ee59f4f952e550163250aba689
leoCottret <71428793+leoCottret@users.noreply.github.com>
2023-03-11 19:09:08 +0100

    add delete todo feature [S07_24]

--- src/first-web-app-05/firstwebapp/src/main/java/com/in28minutes/springboot/firstwebapp/todo/TodoService.java
+++ src/first-web-app-05/firstwebapp/src/main/java/com/in28minutes/springboot/firstwebapp/todo/TodoService.java
@@ -3,6 +3,7 @@ package com.in28minutes.springboot.firstwebapp.todo;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 import org.springframework.stereotype.Service;
 
@@ -26,5 +27,11 @@ public class TodoService {
 
 	public void addTodo(String username, String description, LocalDate targetDate, boolean done) {
 		todos.add(new Todo(++todosCount, username, description, targetDate, done));
+	}
+	
+	public void deleteById(int id) {
+		// lambda function
+		Predicate<? super Todo> predicate = todo -> todo.getId() == id;		
+		todos.removeIf(predicate);
 	}
 }
