362925142b98fe7d328864562d46c7228ae5a944
leoCottret <71428793+leoCottret@users.noreply.github.com>
2023-03-12 13:26:43 +0100

    add TODO bean as an entity for jpa, add SQL script to add data into H2 [S07_36]

--- src/first-web-app-05/firstwebapp/src/main/java/com/in28minutes/springboot/firstwebapp/todo/Todo.java
+++ src/first-web-app-05/firstwebapp/src/main/java/com/in28minutes/springboot/firstwebapp/todo/Todo.java
@@ -2,11 +2,16 @@ package com.in28minutes.springboot.firstwebapp.todo;
 
 import java.time.LocalDate;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Size;
 
-
+@Entity
 public class Todo {
 
+	@Id
+	@GeneratedValue
 	private int id;
 	private String username;
 	@Size(min=10, message="Enter at least 10 characters")
---
+++ src/first-web-app-05/firstwebapp/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into TODO (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values (10001, 'Léo', 'Get AWS Certified', CURRENT_DATE(), false),
+(10002, 'Léo', 'Get Azure Certified', CURRENT_DATE(), false),
+(10003, 'Léo', 'Get GCP Certified', CURRENT_DATE(), false),
+(10004, 'Léo', 'Learn DevOps', CURRENT_DATE(), false);
