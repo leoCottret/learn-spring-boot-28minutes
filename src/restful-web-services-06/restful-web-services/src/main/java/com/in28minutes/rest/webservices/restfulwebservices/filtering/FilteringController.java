fbf9df0bb37a107c2dee43da00166cf3293e6904
leoCottret <71428793+leoCottret@users.noreply.github.com>
2023-03-18 20:46:57 +0100

    add dynamic JSON filtering examples [S08_24]

--- src/restful-web-services-06/restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/filtering/FilteringController.java
+++ src/restful-web-services-06/restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/filtering/FilteringController.java
@@ -3,20 +3,42 @@ package com.in28minutes.rest.webservices.restfulwebservices.filtering;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
 
 @RestController
 public class FilteringController {
 	
 	@GetMapping("/filtering")
-	public SomeBean filtering() {
-		return new SomeBean("val1", "val2", "val3");
+	public MappingJacksonValue filtering() {
+		SomeBean someBean = new SomeBean("val1", "val2", "val3");
+		
+		MappingJacksonValue mappingJacksonValue = new MappingJacksonValue(someBean);
+		// create dynamic JSON filter, here we filter any properties that are not field1 or field3
+		SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("field1", "field3");
+		FilterProvider filters = new SimpleFilterProvider().addFilter("SomeBeanFilter", filter);
+		mappingJacksonValue.setFilters(filters);
+				
+		return mappingJacksonValue;
 	}
 
 	@GetMapping("/filtering-list")
-	public List<SomeBean> filteringList() {
-		return Arrays.asList(new SomeBean("val1", "val2", "val3"), new SomeBean("val3", "val4", "val6"));
+	public MappingJacksonValue filteringList() {
+		MappingJacksonValue mappingJacksonValue = new MappingJacksonValue(Arrays.asList(
+				 new SomeBean("val1", "val2", "val3"), 
+				 new SomeBean("val3", "val4", "val6")
+			));
+		// create dynamic JSON filter, here we filter any properties that are not field1 or field2
+		SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("field1", "field2");
+		FilterProvider filters = new SimpleFilterProvider().addFilter("SomeBeanFilter", filter);
+		mappingJacksonValue.setFilters(filters);
+		
+		return mappingJacksonValue;
 	}
 	
 }
