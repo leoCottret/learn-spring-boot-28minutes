427c875be0ddd64904752c1ae8ff9701e45c53bb
leoCottret <71428793+leoCottret@users.noreply.github.com>
2023-03-18 14:47:23 +0100

    add internationalization with i18n example [S08_19]

--- src/restful-web-services-06/restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/restful-web-services-06/restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,14 +1,18 @@
 package com.in28minutes.rest.webservices.restfulwebservices.helloworld;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
-	
+
+	@Autowired
+	private MessageSource messageSource;
+
 	@GetMapping(path = "/hello-world")
 	public String helloWorld() {
 		return "Hello World";
@@ -18,12 +22,15 @@ public class HelloWorldController {
 	public HelloWorld helloWorldBean() {
 		return new HelloWorld("Hello World");
 	}
-	
-
 
 	@GetMapping(path = "/hello-world/path-variable/{name}")
 	public HelloWorld helloWorldPathVariable(@PathVariable String name) {
 		return new HelloWorld(String.format("Hello World, %s", name));
 	}
-	
+
+	@GetMapping(path = "/hello-world-i18n")
+	public String helloWorldI18n() {
+		return messageSource.getMessage("good.morning.message", null, "Default Message",
+				LocaleContextHolder.getLocale());
+	}
 }
